64190cb05ec9d2d07c5caf1a93e492d293e042db
Unknown <99053034@mydavinci.nl>
2020-04-15 14:13:01 +0200

    Node.js geinstaleerd, Laravel documentatie word document gemaakt

--- resources/views/layouts/basic-layout.blade.php
+++ resources/views/layouts/basic-layout.blade.php
@@ -3,7 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>{{config('app.name', 'MPA-SPA-Framework')}}</title>
+        <title>{{config("app.name", "MPA-SPA-Framework")}}</title>
+        <link rel="stylesheet" href="{{asset("css/app.css")}}">
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
